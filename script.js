e84b3c9c2af39964f1afd3261905ff4325abc065
A Mendez <87824955+AndreMend@users.noreply.github.com>
2021-07-29 18:27:36 +0000

    Rows and Columns and header tags

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-function SearchBar(){
-  var name = prompt ("What is your name?");
-  var number = prompt ("Please enter a number between 1 - 10");
-  var celeb = prompt ("What is your favorite celebrity?");
-  var location = prompt ("What is your favorite location?");
+function SearchBar() {
+  var name = prompt("What is your name?");
+  var number = prompt("Please enter a number between 1 - 10");
+  var celeb = prompt("What is your favorite celebrity?");
+  var location = prompt("What is your favorite location?");
 
-var  result = "Congradulations " + name + " in " + number + " years you will meet " + celeb + " in " + place;
+  var result = "Congradulations " + name + " in " + number + " years you will meet " + celeb + " in " + place;
 
-var image = new Image();
-image.src = "______";
-image.width = "200";
-image.height = "150";
+  var image = new Image();
+  image.src = "______";
+  image.width = "200";
+  image.height = "150";
 
-document.getElementById("resultText").innerHTML = result;
-document.getElementById("resultText").appendChild(image);
+  document.getElementById("resultText").innerHTML = result;
+  document.getElementById("resultText").appendChild(image);
 }
 
-function display(){
+function display() {
   var text = "This is the text I want to display when the button is clicked";
   document.getElementbyId("displayText").innerHTML = text;
+}
+
+function signUp() {
+  var username = prompt("Please Enter Your Name?");
+  if (username == "") {
+    document.getElementById ("information").innerHTML = "You have not signed up";
+  }
+  else {
+    var signedUp = "Thank you" + username + " for signing up ";
+document.getElementById("information").innerHTMl = signedUp;
+  }
 }

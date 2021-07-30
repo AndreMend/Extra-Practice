d70b7a223848863afb60da90da1c53b7d00cc68b
A Mendez <87824955+AndreMend@users.noreply.github.com>
2021-07-30 17:54:23 +0000

    css

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-function SearchBar() {
+function bar () {
   var name = prompt("What is your name?");
   var number = prompt("Please enter a number between 1 - 10");
   var celeb = prompt("What is your favorite celebrity?");
   var location = prompt("What is your favorite location?");
 
-  var result = "Congradulations " + name + " in " + number + " years you will meet " + celeb + " in " + place;
+  var result = "Congradulations " + name + " in " + number + " years you will meet " + celeb + " in " + location;
 
   var image = new Image();
-  image.src = "______";
+  image.src = "Logo.png";
   image.width = "200";
   image.height = "150";
 
   document.getElementById("resultText").innerHTML = result;
-  document.getElementById("resultText").appendChild(image);
+  
+  document.getElementById("resultImage").appendChild(image);
 }
 
 function display() {
   var text = "This is the text I want to display when the button is clicked";
-  document.getElementbyId("displayText").innerHTML = text;
+  document.getElementById("displayText").innerHTML = text;
 }
 
-function signUp() {
+function signUp(){
   var username = prompt("Please Enter Your Name?");
   if (username == "") {
     document.getElementById ("information").innerHTML = "You have not signed up";
   }
   else {
     var signedUp = "Thank you" + username + " for signing up ";
-document.getElementById("information").innerHTMl = signedUp;
+    
+    document.getElementById("information").innerHTML = signedUp;
+  }
+}
+
+function ourAnimation(){
+  var ourImage = document.getElementById("animations");
+
+  ourImage.style.position = "relative";
+  var position = 0;
+
+  setInterval(move, 5);
+
+  function move(){
+    if(position == window.screen.width){
+      clearInterval();   
+    }
+    else{
+      position++;
+      ourImage.style.left = position + "px";
+    }
   }
 }
